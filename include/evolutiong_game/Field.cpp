532390485c3e9b29d11341a7f2ef90b250596ebb
ivantretyak <das.blackya@gmail.com>
2021-05-11 12:44:09 +0300

    Incapsultion some methods

--- include/evolutiong_game/Field.cpp
+++ include/evolutiong_game/Field.cpp
@@ -41,34 +41,28 @@ void Field::move() {
             switch (field[y][x].getType()) {
                 case cell_hebivor:
                 case cell_predator: {
-                    if (std::get<Cell>(field[y][x].getItem()).death()) {
+                    Cell *cell = &std::get<Cell>(field[y][x].getItem());
+                    if (cell->death()) {
                         field[y][x].changeSection(Food(meat, 100));
                         break;
                     }
-                    if (std::get<Cell>(field[y][x].getItem()).getMoving()) {
+                    if (cell->getMoving()) {
                         break;
                     }
-                    std::get<Cell>(field[y][x].getItem()).switchMoving();
-                    Coordinate currentCoordinate = std::get<Cell>(field[y][x].getItem()).getCoordinate();
-                    std::vector<int> upC = currentCoordinate.getNewCoordinate(size, up);
-                    std::vector<int> downC = currentCoordinate.getNewCoordinate(size, down);
-                    std::vector<int> rightC = currentCoordinate.getNewCoordinate(size, right);
-                    std::vector<int> leftC = currentCoordinate.getNewCoordinate(size, left);
-
-                    SectionType upT = field[upC[1]][upC[0]].getType();
-                    SectionType downT = field[downC[1]][downC[0]].getType();
-                    SectionType rightT = field[rightC[1]][rightC[0]].getType();
-                    SectionType leftT = field[leftC[1]][leftC[0]].getType();
-
-                    Coordinate newCoordinate = std::get<Cell>(field[y][x].getItem()).move(upT, leftT, rightT, downT,
-                                                                                          size);\
+                    cell->switchMoving();
+                    Coordinate currentCoordinate = cell->getCoordinate();
+                    SectionType upT;
+                    SectionType downT;
+                    SectionType rightT;
+                    SectionType leftT;
+                    getAround(currentCoordinate, upT, downT, rightT, leftT);
+                    Coordinate newCoordinate = cell->move(upT, leftT, rightT, downT);
                     if (newCoordinate.getXY()[0] == -1 && newCoordinate.getXY()[1] == -1) {
                         break;
                     }
 
                     if (newCoordinate.getXY()[0] == x && newCoordinate.getXY()[1] == y) {
-                        Cell newCell = std::get<Cell>(field[y][x].getItem()).reproduction(upT, leftT, rightT, downT,
-                                                                                          size);
+                        Cell newCell = cell->reproduction(upT, leftT, rightT, downT);
                         auto xy = newCell.getCoordinate().getXY();
                         field[xy[1]][xy[0]].changeSection(newCell);
                         break;
@@ -77,31 +71,18 @@ void Field::move() {
                     std::vector<int> newXY = newCoordinate.getXY();
                     switch (field[newXY[1]][newXY[0]].getType()) {
                         case empty: {
-                            field[newXY[1]][newXY[0]].changeSection(std::get<Cell>(field[y][x].getItem()));
-                            std::get<Cell>(field[newXY[1]][newXY[0]].getItem()).changeCoordinate(newCoordinate);
-                            field[y][x].erase();
+                            moving(y, x, newCoordinate, newXY);
                             break;
                         }
-                        case food_grass: {
-                            std::get<Cell>(field[y][x].getItem()).eat(
-                                    std::get<Food>(field[newXY[1]][newXY[0]].getItem()).getEnergy());
-                            field[newXY[1]][newXY[0]].changeSection(std::get<Cell>(field[y][x].getItem()));
-                            std::get<Cell>(field[newXY[1]][newXY[0]].getItem()).changeCoordinate(newCoordinate);
-                            field[y][x].erase();
+                        case food_grass:
+                        case food_meat:
+                            eating(y, x, newXY);
+                            moving(y, x, newCoordinate, newXY);
                             break;
-                        }
-                        case food_meat: {
-                            std::get<Cell>(field[y][x].getItem()).eat(
-                                    std::get<Food>(field[newXY[1]][newXY[0]].getItem()).getEnergy());
-                            field[newXY[1]][newXY[0]].changeSection(std::get<Cell>(field[y][x].getItem()));
-                            std::get<Cell>(field[newXY[1]][newXY[0]].getItem()).changeCoordinate(newCoordinate);
-                            field[y][x].erase();
-                            break;
-                        }
+
                         case cell_hebivor:
                         case cell_predator:
-                            std::get<Cell>(field[newXY[1]][newXY[0]].getItem()).hit(
-                                    std::get<Cell>(field[y][x].getItem()).damage());
+                            hiting(y, x, newXY);
                             break;
                     }
                 }
@@ -109,12 +90,7 @@ void Field::move() {
                 case food_grass:
                     break;
                 case empty: {
-                    std::random_device rs;
-                    std::uniform_int_distribution<> types(0, 100);
-                    int chance = types(rs);
-                    if (chance < 5) {
-                        field[y][x].changeSection(Food(grass, 25));
-                    }
+                    createFood(y, x);
                     break;
                 }
 
@@ -123,13 +99,55 @@ void Field::move() {
             }
         }
     }
+    switchingMoving();
+}
+
+void Field::switchingMoving() {
     for (int y = 0; y < size; y++) {
         for (int x = 0; x < size; x++) {
-            if (field[y][x].getType() == cell_predator or field[y][x].getType() == cell_hebivor) {
+            if (field[y][x].getType() == cell_predator || field[y][x].getType() == cell_hebivor) {
                 std::get<Cell>(field[y][x].getItem()).switchMoving();
             }
         }
     }
+}
+
+void Field::getAround(Coordinate &currentCoordinate, SectionType &upT, SectionType &downT, SectionType &rightT,
+                      SectionType &leftT) {
+    std::vector<int> downC = currentCoordinate.getNewCoordinate(down);
+    std::vector<int> rightC = currentCoordinate.getNewCoordinate(right);
+    std::vector<int> leftC = currentCoordinate.getNewCoordinate(left);
+    std::vector<int> upC = currentCoordinate.getNewCoordinate(up);
+
+    upT= field[upC[1]][upC[0]].getType();
+    downT= field[downC[1]][downC[0]].getType();
+    rightT= field[rightC[1]][rightC[0]].getType();
+    leftT= field[leftC[1]][leftC[0]].getType();
+}
+
+void Field::createFood(int y, int x) {
+    std::random_device rs;
+    std::uniform_int_distribution<> types(0, 100);
+    int chance = types(rs);
+    if (chance < 5) {
+        field[y][x].changeSection(Food(grass, 25));
+    }
+}
+
+void Field::hiting(int y, int x, const std::vector<int> &newXY) {
+    std::get<Cell>(field[newXY[1]][newXY[0]].getItem()).hit(
+            std::get<Cell>(field[y][x].getItem()).damage());
+}
+
+void Field::eating(int y, int x, const std::vector<int> &newXY) {
+    std::get<Cell>(field[y][x].getItem()).eat(
+            std::get<Food>(field[newXY[1]][newXY[0]].getItem()).getEnergy());
+}
+
+void Field::moving(int y, int x, Coordinate &newCoordinate, const std::vector<int> &newXY) {
+    field[newXY[1]][newXY[0]].changeSection(std::get<Cell>(field[y][x].getItem()));
+    std::get<Cell>(field[newXY[1]][newXY[0]].getItem()).changeCoordinate(newCoordinate);
+    field[y][x].erase();
 }
 
 long int Field::getSteps() const {
--- include/evolutiong_game/Field.h
+++ include/evolutiong_game/Field.h
@@ -4,6 +4,7 @@
 
 #ifndef GENES_FIELD_H
 #define GENES_FIELD_H
+
 #include "Section.h"
 #include <string>
 #include <iostream>
@@ -13,11 +14,29 @@ private:
     std::vector<std::vector<Section>> field;
     unsigned int size;
     long int steps = 0;
+
+    void moving(int y, int x, Coordinate &newCoordinate, const std::vector<int> &newXY);
+
+    void eating(int y, int x, const std::vector<int> &newXY);
+
+    void hiting(int y, int x, const std::vector<int> &newXY);
+
+    void createFood(int y, int x);
+
+    void
+    getAround(Coordinate &currentCoordinate, SectionType &upT, SectionType &downT, SectionType &rightT,
+              SectionType &leftT);
+
 public:
     Field(unsigned int s);
+
     void move();
+
     long int getSteps() const;
+
     std::vector<std::vector<Section>> getField();
+
+    void switchingMoving();
 };
 
 
